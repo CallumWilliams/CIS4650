778ae81aad991d763ecbb93dfefdece8d5e4a500
Callum Williams <cwilli17@mac-6d73.local>
2017-03-03 12:37:47 -0500

    reformatting

--- absyn/OpExp.java
+++ absyn/OpExp.java
@@ -1,13 +1,13 @@
 package absyn;
 
-public class OpExp extends Exp 
+public class OpExp extends Exp
 {
   public final static int PLUS  = 0;
   public final static int MINUS = 1;
   public final static int MUL   = 2;
   public final static int DIV   = 3;
-  public final static int EQ    = 4;
-  public final static int NE    = 5;  
+  public final static int EQ    = 4; //==
+  public final static int NE    = 5;
   public final static int LT    = 6;
   public final static int LE    = 7;
   public final static int GT    = 8;
@@ -16,8 +16,8 @@ public class OpExp extends Exp
   public Exp left;
   public int op;
   public Exp right;
-  
-  public OpExp(int pos, Exp left, int op, Exp right) 
+
+  public OpExp(int pos, Exp left, int op, Exp right)
   {
     this.pos = pos;
     this.left = left;
@@ -25,6 +25,3 @@ public class OpExp extends Exp
     this.right = right;
   }
 }
-
-
-
