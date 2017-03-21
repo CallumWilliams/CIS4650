0df4b4e08bdfc8f947e7502b1f9cb3c7189de2c8
Callum Williams <callum.a.williams@gmail.com>
2017-03-21 12:22:35 -0400

    Symbol table generation

--- cm.java
+++ cm.java
@@ -1,25 +1,30 @@
 import java.io.*;
-import java.util.HashMap;
-   
+import java.util.*;
 class cm {
 
   public static Boolean displayTree = false;
-  public static HashMap<String, Integer> hm = new HashMap<String, Integer>();
-  static public void main(String argv[]) {    
+  public static Boolean displaySmybolTable = false;
+  
+  public static HashMap<String, ArrayList<String>> hm = new HashMap<String, ArrayList<String>>();
+  public static ArrayList<String> int_list = new ArrayList<String>();
+  public static ArrayList<String> void_list = new ArrayList<String>();
+  
+  static public void main(String argv[]) { 
   
     for(String s : argv)
     {
-        if(s.equals("-a"))
-        {
-            displayTree = true;
-        }   
+        if(s.equals("-a")) displayTree = true;
+        else if (s.equals("-s")) displaySmybolTable = true;
     }
 
     try 
     {
       parser p = new parser(new Lexer(new FileReader(argv[0])));
       Object result = p.parse().value;
-      System.out.println(hm.size());
+      hm.put("int", int_list);
+      hm.put("void", void_list);
+      System.out.println(hm.keySet().toString());
+      System.out.println(hm.values().toString());
 
     } catch (Exception e) {
       /* do cleanup here -- possibly rethrow e */
